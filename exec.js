fe6321a2f6b86f1ac5408429fa0ba311aa855f1d
LacamJC <joaoramajo744@gmail.com>
2024-10-13 20:59:04 -0300

    corrigindo script

--- exec.js
+++ exec.js
@@ -4,9 +4,8 @@ const readline = require('readline-sync');
 function commitAndPush(){
     const msg = readline.question("Mensagem de commit: ");
     shell.exec('git add *');
-    shell.exec(`git commit -am '${msg}'`);
+    shell.exec(`git commit -am "${msg}"`);
     shell.exec('git push');
-
     console.log('Alterações realizadas com sucesso')
 }
 
