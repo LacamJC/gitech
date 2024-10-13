999cb2926ad2f7e3cc8e3366ca364442de90d001
LacamJC <joaoramajo744@gmail.com>
2024-10-13 20:56:19 -0300

    Adicionando

--- exec.js
+++ exec.js
@@ -1,12 +1,13 @@
 const shell = require('shelljs');
+const readline = require('readline-sync');
 
-function commitAndPush(msg){
+function commitAndPush(){
+    const msg = readline.question("Mensagem de commit: ");
     shell.exec('git add *');
-    shell.exec('console.log("Adicionando todas as modificações para o git"');
     shell.exec(`git commit -am '${msg}'`);
     shell.exec('git push');
 
     console.log('Alterações realizadas com sucesso')
 }
 
-commitAndPush('teste')
+commitAndPush()
