4b912087e7149930848bd253391298f9fda5e3e2
LacamJC <joaoramajo744@gmail.com>
2024-10-13 21:13:02 -0300

    A

--- exec.js
+++ exec.js
@@ -1,8 +1,9 @@
-const shell = require('shelljs');
+// Importe as dependencias necessarias com shelljs com 'npm i shelljs readline-sync
+const shell = require('shelljs'); 
 const readline = require('readline-sync');
 
 function commitAndPush(){
-    const msg = readline.question("Mensagem de commit: ");
+    const msg = readline.question("Mensagem de commit: "); // Mensagem para o commit
     shell.exec('git add *');
     shell.exec(`git commit -am "${msg}"`);
     shell.exec('git push');
